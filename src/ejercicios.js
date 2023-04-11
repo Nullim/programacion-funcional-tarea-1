7026e4ad62cde1209480e5ffa1068379a24c760a
Viper292 <crazymelon@hotmail.es>
2023-04-11 05:39:38 -0300

    Agrega tareas

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,19 +1,69 @@
-export function multiplicarPor10(array) {}
+export function multiplicarPor10(array) {
+    const nuevoArrayMultiplicado = array.map(numero => numero * 10);
+    return nuevoArrayMultiplicado;
+}
 
-export function soloPares(array) {}
+export function soloPares(array) {
+    const arrayConSoloPares = array.filter(numero => numero % 2 === 0);
+    return arrayConSoloPares;
+}
 
-export function moverALaDerecha(array) {}
+export function moverALaDerecha(array) {
+    const ultimo = array[array.length - 1];
+    const arrayModificado = [...array.slice(0, array.length - 1)];
+    arrayModificado.unshift(ultimo);
+    return arrayModificado;
+}
 
-export function soloUnaPalabra(array) {}
+export function soloUnaPalabra(array) {
+    const arraySinEspacios = array.filter(palabra => !palabra.includes(' '));
+    return arraySinEspacios;
+}
 
-export function soloArraysPositivos(array) {}
+export function soloArraysPositivos(array) {
+    return array.filter(arrayConNumeros => {
+        return arrayConNumeros.every(numero => numero > 0);
+    });
+}
 
-export function soloVocales(array) {}
+export function soloVocales(array) {
+    const vocales = ['a', 'e', 'i', 'o', 'u'];
+    const arraySinConsonantes = array.map(palabra => {
+        const palabraSeparada = palabra.split('');
+        const palabraSinConsonantes = palabraSeparada.filter(letra => vocales.includes(letra)).join('');
+        return palabraSinConsonantes;
+    });
+    return arraySinConsonantes;
+}
 
-export function todasVocalesIguales(array) {}
+export function todasVocalesIguales(array) {
+    return array.filter(palabra => {
+        const vocalesIguales = palabra.toLowerCase().match(/[aeiou]/g);
+        if (vocalesIguales.every(letra => letra === vocalesIguales[0])) {
+            return true;
+        }
+        return false;
+    });
+}
 
-export function duplicarMatriz(array) {}
+export function duplicarMatriz(array) {
+    return array.map(arrayParaMultiplicar => {
+        return arrayParaMultiplicar.map(numero => numero * 2);
+    });
+}
 
-export function mayoresDeEdadValidados(array) {}
+export function mayoresDeEdadValidados(array) {
+    return array.map(persona => {
+        return {
+            edad: persona.edad,
+            mayor: persona.edad > 18 ? true : false,
+            nombre: persona.nombre
+        };
+    });
+}
 
-export function transformarObjetoEnArray(objeto) {}
+export function transformarObjetoEnArray(objeto) {
+    return Object.entries(objeto).map(([key, value]) => {
+        return { id: key, ...value};
+    });
+}
